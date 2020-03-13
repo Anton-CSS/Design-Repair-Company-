6e702430810119e12b1948b2f1ab07312f14bd5c
Anton-CSS <9251580262@mail.ru>
2020-03-13 19:25:54 +0300

    lesson21

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,70 @@
+
+/* Основное задание 20 урок
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  const mobal = document.querySelector('.mobal');
+  const mobalBtn = document.querySelectorAll('[data-toggle=mobal]');
+  const closeBtn = document.querySelector('.mobal__close');
+  const switchMobal = () =>{
+      mobal.classList.toggle('mobal--visible');
+   }
+     mobalBtn.forEach(element =>{
+    element.addEventListener('click', switchMobal);
+  });
+   closeBtn.addEventListener('click', switchMobal);
+ });*/
+
+/* Усложненное задание 20 урок
+
 document.addEventListener("DOMContentLoaded", function(event) {
   const mobal = document.querySelector('.mobal');
   const mobalBtn = document.querySelectorAll('[data-toggle=mobal]');
   const closeBtn = document.querySelector('.mobal__close')
+  const dark = document.querySelector('.wrap')
   const switchMobal = () =>{
      mobal.classList.toggle('mobal--visible');
+     document.addEventListener('keyup', function(e){
+  let key = e.keyCode;
+  if(key == 27) {
+    mobal.classList.toggle('mobal--visible');
+    switchMobal();
   }
-  
+  })
+  dark.classList.toggle('wrap--dark');
+  }
+
   mobalBtn.forEach(element =>{
     element.addEventListener('click', switchMobal);
   });
   closeBtn.addEventListener('click', switchMobal);
+  dark.addEventListener('click', switchMobal);
+}); */
+
+$(document).ready(function (){
+  var mobal = $('.mobal'),
+      mobalBtn = $('[data-toggle=mobal]'),
+      closeBtn = $('.mobal__close');
+
+      mobalBtn.on('click', function(){
+        mobal.toggleClass('mobal--visible');
+      });
+      closeBtn.on('click', function(){
+        mobal.toggleClass('mobal--visible');
+      })
+});
+
+
+$(function(){
+	$(window).scroll(function(){
+		if($(window).scrollTop() > 100) {
+			$('#scroll_top').show();
+		} else {
+			$('#scroll_top').hide();
+		}
+	});
+ 
+	$('#scroll_top').click(function(){
+		$('html, body').animate({scrollTop: 0}, 600);
+		return false;
+	});
 });
