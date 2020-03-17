83bf87a5b6b959e5c90b077f5c2774add7a81fcf
Anton-CSS <9251580262@mail.ru>
2020-03-17 14:38:09 +0300

    lesson_23

--- src/js/main.js
+++ src/js/main.js
@@ -56,18 +56,25 @@ $(document).ready(function (){
     pagination: {
       el: '.swiper-pagination',
       type: 'bullets',
+      
     },
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
   })
+  
   var next = $('.swiper-button-next');
   var prev = $('.swiper-button-prev');
   var bullets = $('.swiper-pagination');
+
   next.css('left', prev.width() + 10 + bullets.width() + 10 )
   bullets.css('left',prev.width() + 10  )
+
+  new WOW().init();
 });
+
+
 
 
 $(function(){
